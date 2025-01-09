a0416a4d7669f243b0d8c54774affbe8301a4127
Thomas Perger <pt7063@mci4me.at>
2025-01-09 22:46:46 +0100

    updates .math

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import math
+
 def calculate_angles(x, y, z):
     # Berechne Pitch und Roll
-    pitch = degrees(atan2(x, sqrt(y**2 + z**2)))
-    roll = degrees(atan2(y, sqrt(x**2 + z**2)))
+    pitch = math.degrees(math.atan2(x, math.sqrt(y**2 + z**2)))
+    roll = math.degrees(math.atan2(y, math.sqrt(x**2 + z**2)))
     return pitch, roll
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from machine import I2C, Pin
-from adxl345 import ADXL345_I2C
+from ADXL345 import ADXL345_I2C
 from functions import calculate_angles
 import math
+import time
 
 #I2C initialisieren
 i2c = I2C(0,sda=Pin(21), scl=Pin(22))
 imu = ADXL345_I2C(i2c)
 
 while True:
-    # Beschleunigungswerte abrufen
-    x, y, z = imu.acceleration
-    
+    # Beschleunigungswerte auslesen
+    x = imu.xValue
+    y = imu.yValue
+    z = imu.zValue
     # Winkel berechnen
     pitch, roll = calculate_angles(x, y, z)
     
     # Ausgabe
     print(f"Pitch: {pitch:.2f}°, Roll: {roll:.2f}°")
+
+    time.sleep(10)
